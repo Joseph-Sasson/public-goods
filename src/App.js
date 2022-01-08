f5a9dae1399eeffe88a928f90a1378523e08c2a3
Joseph <josephsasson1@gmail.com>
2022-01-07 22:38:36 -0500

    small changes

--- src/App.js
+++ src/App.js
@@ -5,27 +5,27 @@ function App() {
     <div className="App">
       <div className="top-div">
         <h1>Treat your pet (and you).</h1>
-        <h1>It's on us!</h1>
+        <h1>It"s on us!</h1>
         <p>Treat your pet to a healthy treat for <strong>free</strong> to kick off your Public Goods trial membership.</p>
-        <h3>See what it's like to be a Public Goods member.</h3>
+        <h3>See what it"s like to be a Public Goods member.</h3>
         <ul>
           <li>Included in your bundle is a free 2-week membership that gives you unlimited access to our entire collection of sustainable essentials.</li>
           <li>Take a load off. We make it easy for you to make better choices. Always included: eco-friendly products you can trust</li>
         </ul>
         <button>Claim your free offer</button>
         <div className="img-div">
-          <img alt='Cat wanting treats' src="/images/cat-desktop-1.png" className="img-one"/>
+          <img alt="Cat wanting treats" src="/images/cat-desktop-1.png" className="img-one"/>
         </div>
       </div>
       <div className="middle-div">
         <h3>OUR TREAT</h3>
         <div>
-          <img alt='Cat treats' src="/images/cat-treats-minnows.png" className="treats"/>
+          <img alt="Cat treats" src="/images/cat-treats-minnows.png" className="treats"/>
           <h3>Freeze Dried Minnows</h3>
           <p>Healthy ingredients with nothing to hide.</p>
         </div>
         <div>
-          <img alt='Cat treats' src="/images/cat-treats-minnows.png" className="treats"/>
+          <img alt="Cat treats" src="/images/cat-treats-minnows.png" className="treats"/>
           <h3>Freeze Dried Minnows</h3>
           <p>Healthy ingredients with nothing to hide.</p>
         </div>
